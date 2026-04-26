a3c802ad2aac0f61640cf84f2cb0b9ea8cb408bb
gurkandonbaloglu <gdonbaloglu@hacettepe.edu.tr>
2026-04-26 23:11:15 +0300

    Update instrdec.sv

--- instrdec.sv
+++ instrdec.sv
@@ -5,13 +5,13 @@ module instrdec (
 
     always_comb begin
         case (op)
-            7'b0110011: ImmSrc = 2'b00; // R-type (Normalde xx, kurala göre 00 yaptık)
+            7'b0110011: ImmSrc = 2'b00; // R-type 
             7'b0010011: ImmSrc = 2'b00; // I-type ALU
             7'b0000011: ImmSrc = 2'b00; // lw
             7'b0100011: ImmSrc = 2'b01; // sw
             7'b1100011: ImmSrc = 2'b10; // beq
             7'b1101111: ImmSrc = 2'b11; // jal
-            default:    ImmSrc = 2'b00; // Tanımsız durumlar için 00
+            default:    ImmSrc = 2'b00; 
         endcase
     end
 
